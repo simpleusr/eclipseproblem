541f447def600629a766a48c49b7b85a96d9209d
simpleusr <ceyhankasap@gmail.com>
2020-05-23 22:11:54 +0300

    initial without cycle

---
+++ projecta/src/main/java/com/mycompany/eclipseproblem/projecta/ClassFromProjectA.java
@@ -0,0 +1,12 @@
+package com.mycompany.eclipseproblem.projecta;
+
+//import com.mycompany.eclipseproblem.projectb.ClassFromProjectB;
+
+public class ClassFromProjectA 
+{
+	
+//	public static void methodOfClassFromProjectA(ClassFromProjectB arg) {
+//		
+//	}
+ 
+}
---
+++ projectb/src/main/java/com/mycompany/eclipseproblem/projectb/ClassFromProjectB.java
@@ -0,0 +1,11 @@
+package com.mycompany.eclipseproblem.projectb;
+//import com.mycompany.eclipseproblem.projecta.ClassFromProjectA;
+
+public class ClassFromProjectB 
+{
+	
+//	public static void methodOfClassFromProjectB(ClassFromProjectA arg) {
+//		
+//	}
+ 
+}
